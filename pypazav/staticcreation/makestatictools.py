cffd0685cd1c4ad72e0af5a8631a25f979f9faac
stefanfluck <58817296+stefanfluck@users.noreply.github.com>
2020-01-30 10:23:26 +0100

    created makestatictools.py

---
+++ pypazav/staticcreation/makestatictools.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 30 10:13:51 2020
+STATIC CREATION FILE TURNED INTO FUNCTIONS
+
+"""
+
+def loadascgeodata(array):
+    #use after applying geodatatools which output an array.
