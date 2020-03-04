7d5b20ea9d658d84a84316658c72bd2584e028ea
stefanfluck <58817296+stefanfluck@users.noreply.github.com>
2020-03-04 16:44:24 +0100

    added createzlad(), added RAW CODE of lad generation. functionalize it.

--- palmpy/postprocessing/posttools.py
+++ palmpy/postprocessing/posttools.py
@@ -39,40 +39,40 @@ def plotxz_quiv(filein, fileout, seltime, originz, y,
 
     Parameters
     ----------
-    filein : TYPE
-        DESCRIPTION.
-    fileout : TYPE
-        DESCRIPTION.
-    seltime : TYPE
-        DESCRIPTION.
-    originz : TYPE
-        DESCRIPTION.
-    y : TYPE
-        DESCRIPTION.
-    colvar : TYPE, optional
-        DESCRIPTION. The default is 'w'.
-    xslice : TYPE
-        DESCRIPTION.
-    zslice : TYPE
-        DESCRIPTION.
-    plotsize : TYPE, optional
-        DESCRIPTION. The default is (7.2,4).
-    cbarspan : TYPE, optional
-        DESCRIPTION. The default is [0,5].
-    title : TYPE, optional
-        DESCRIPTION. The default is 'Title'.
-    cbarlabel : TYPE, optional
-        DESCRIPTION. The default is 'Magnitude'.
-    quivdens : TYPE, optional
-        DESCRIPTION. The default is 10.
-    saveflag : TYPE, optional
-        DESCRIPTION. The default is 0.
-    dpi : TYPE, optional
-        DESCRIPTION. The default is 300.
-    qual : TYPE, optional
-        DESCRIPTION. The default is 100.
-    closeafter : TYPE, optional
-        DESCRIPTION. The default is False.
+
+    filein : str
+        Input netcdf file.
+    fileout : str
+        output filename with file ending.
+    seltime : str
+        time since simulation start, eg. '03:00:00' for third hour of simulation.
+    originz : float
+        shift of model domain in z direction that was applied in preprocessing.
+    y : float
+        position of slice in y direction.
+    colvar : str, optional
+        variable to be plotted. The default is 'w'.
+    xslice : list
+        range of x to be plotted, [xmin, xmax]. For whole range, issue [-1,-1],
+    zslice : list
+        range of z to be plotted, [zmin, zmax]. For whole range, issue [-1,-1],
+    plotsize : tuple, optional
+        figure size. The default is (7.2,4).
+    cbarspan : list, optional
+        Colorbar center and max value. Leads to symmetrical color map. The default is [0,5].
+    title : str, optional
+        Case Title. The default is 'Title'.
+    quivdens : int, optional
+        quiver density in plot (higher -> less quivers plotted). The default is 10.
+    saveflag : bool, optional
+        save flag. If true, it saves the figure. The default is 0.
+    dpi : int, optional
+        save fig with this dpi. The default is 300.
+    qual : int, optional
+        save fig with this jpg quality. The default is 100.
+    closeafter : bool, optional
+        If True, the figure is closed after saving. The default is False.
+
 
     Returns
     -------
@@ -163,45 +163,45 @@ def plotxz_quiv(filein, fileout, seltime, originz, y,
 
 
 
-def plotxz(filein, fileout, seltime, originz, y, colvar='w',
-                xslice, zslice, plotsize=(7.2,4), cbarspan=[0,5],
-                title='Title',
+def plotxz(filein, fileout, seltime, originz, y, xslice, zslice,
+                colvar='w', plotsize=(7.2,4), cbarspan=[0,5], title='Title',
                 dpi=300, qual=100, closeafter=False):
- '''
+    '''
     plot xz slice without quiver, good old coloring.
 
     Parameters
     ----------
-    filein : TYPE
-        DESCRIPTION.
-    fileout : TYPE
-        DESCRIPTION.
-    seltime : TYPE
-        DESCRIPTION.
-    originz : TYPE
-        DESCRIPTION.
-    y : TYPE
-        DESCRIPTION.
-    colvar : TYPE, optional
-        DESCRIPTION. The default is 'w'.
-    xslice : TYPE
-        DESCRIPTION.
-    zslice : TYPE
-        DESCRIPTION.
-    plotsize : TYPE, optional
-        DESCRIPTION. The default is (7.2,4).
-    cbarspan : TYPE, optional
-        DESCRIPTION. The default is [0,5].
-    title : TYPE, optional
-        DESCRIPTION. The default is 'Title'.
-    saveflag : TYPE, optional
-        DESCRIPTION. The default is 0.
-    dpi : TYPE, optional
-        DESCRIPTION. The default is 300.
-    qual : TYPE, optional
-        DESCRIPTION. The default is 100.
-    closeafter : TYPE, optional
-        DESCRIPTION. The default is False.
+    filein : str
+        Input netcdf file.
+    fileout : str
+        output filename with file ending.
+    seltime : str
+        time since simulation start, eg. '03:00:00' for third hour of simulation.
+    originz : float
+        shift of model domain in z direction that was applied in preprocessing.
+    y : float
+        position of slice in y direction.
+    colvar : str, optional
+        variable to be plotted. The default is 'w'.
+    xslice : list
+        range of x to be plotted, [xmin, xmax]. For whole range, issue [-1,-1],
+    zslice : list
+        range of z to be plotted, [zmin, zmax]. For whole range, issue [-1,-1],
+    plotsize : tuple, optional
+        figure size. The default is (7.2,4).
+    cbarspan : list, optional
+        Colorbar center and max value. Leads to symmetrical color map. The default is [0,5].
+    title : str, optional
+        Case Title. The default is 'Title'.
+        saveflag : bool, optional
+        save flag. If true, it saves the figure. The default is 0.
+    dpi : int, optional
+        save fig with this dpi. The default is 300.
+    qual : int, optional
+        save fig with this jpg quality. The default is 100.
+    closeafter : bool, optional
+        If True, the figure is closed after saving. The default is False.
+
 
     Returns
     -------
@@ -285,49 +285,47 @@ def plotxz(filein, fileout, seltime, originz, y, colvar='w',
 
 
 
-def plotxz_contourf(filein, fileout, seltime, originz, y, colvar='w',
-                xslice, zslice, plotsize=(7.2,4), cbarspan=[0,5], levels=25,
+def plotxz_contourf(filein, fileout, seltime, originz, y, xslice, zslice, 
+                colvar='w', plotsize=(7.2,4), cbarspan=[0,5], levels=25,
                 title='Title', saveflag=0,
                 dpi=300, qual=100, closeafter=False):
- '''
+    '''
     plot xz slice without quiver, good old coloring.
 
     Parameters
     ----------
-    filein : TYPE
-        DESCRIPTION.
-    fileout : TYPE
-        DESCRIPTION.
-    seltime : TYPE
-        DESCRIPTION.
-    originz : TYPE
-        DESCRIPTION.
-    y : TYPE
-        DESCRIPTION.
-    colvar : TYPE, optional
-        DESCRIPTION. The default is 'w'.
-    xslice : TYPE
-        DESCRIPTION.
-    zslice : TYPE
-        DESCRIPTION.
-    plotsize : TYPE, optional
-        DESCRIPTION. The default is (7.2,4).
-    cbarspan : TYPE, optional
-        DESCRIPTION. The default is [0,5].
-    levels : TYPE, optional
-        DESCRIPTION. The default is 25.
-    title : TYPE, optional
-        DESCRIPTION. The default is 'Title'.
-    cbarlabel : TYPE, optional
-        DESCRIPTION. The default is 'Magnitude'.
-    saveflag : TYPE, optional
-        DESCRIPTION. The default is 0.
-    dpi : TYPE, optional
-        DESCRIPTION. The default is 300.
-    qual : TYPE, optional
-        DESCRIPTION. The default is 100.
-    closeafter : TYPE, optional
-        DESCRIPTION. The default is False.
+    filein : str
+        Input netcdf file.
+    fileout : str
+        output filename with file ending.
+    seltime : str
+        time since simulation start, eg. '03:00:00' for third hour of simulation.
+    originz : float
+        shift of model domain in z direction that was applied in preprocessing.
+    y : float
+        position of slice in y direction.
+    colvar : str, optional
+        variable to be plotted. The default is 'w'.
+    xslice : list
+        range of x to be plotted, [xmin, xmax]. For whole range, issue [-1,-1],
+    zslice : list
+        range of z to be plotted, [zmin, zmax]. For whole range, issue [-1,-1],
+    plotsize : tuple, optional
+        figure size. The default is (7.2,4).
+    cbarspan : list, optional
+        Colorbar center and max value. Leads to symmetrical color map. The default is [0,5].
+    levels : int, optional
+        how many levels in the colorbar. The default is 25.
+    title : str, optional
+        Case Title. The default is 'Title'.
+        saveflag : bool, optional
+        save flag. If true, it saves the figure. The default is 0.
+    dpi : int, optional
+        save fig with this dpi. The default is 300.
+    qual : int, optional
+        save fig with this jpg quality. The default is 100.
+    closeafter : bool, optional
+        If True, the figure is closed after saving. The default is False.
 
     Returns
     -------
