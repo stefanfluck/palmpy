6443c0fa1dd7ce6013f715ea713822888fd7146b
stefan fluck <58817296+stefanfluck@users.noreply.github.com>
2021-09-22 15:20:20 +0200

    modified dm01avzh24.py dictfile

--- palmpy/staticcreation/dictfolder/DM01AVZH24.py
+++ palmpy/staticcreation/dictfolder/DM01AVZH24.py
@@ -5,64 +5,66 @@ MAPPING DICTIONARIES BETWEEN DIFFERENT CLASSIFICATIONS TO PALM CLASSES
 - SOIL CLASSES
 - PAVEMENT CLASSES
 
-by Stefan Fluck, 24.06.2020
-Usable with Stadt Zürichs Geodaten (Amtl. Vermessungsdaten)
+by Stefan Fluck, 22.09.2021
+Created for DM01AVZH24 data standard. Standard is similar to federal standard DM01AV, but with 
+some special traits for Zurich (and eastern Switzerland). Mapping to PALM-Classes as a best guess.
+
 
 '''
 
 import palmpy.staticcreation.makestatictools as mst
 
-#%% BB 2 PALM
-#  BB vegetation classes to PALM vegetation types
-bb2palmveg = {   21:3,
-                 22:2,
-                 23:2,
-                 24:3,
-                 25:3,
-                 26:3,
-                 27:3,
-                 28:14,
-                 29:3,
-                 31:3,
-                 35:17,
-                 38:16,
-               1001:3,
-               1002:2,
-               1003:1,
-               1004:8,
-               1005:3,
-               1006:2,
-               1007:2,
-               2000:3,
-               2001:10,
+#%% Land Use 2 PALM types
+#  land use vegetation classes to PALM vegetation types
+bb2palmveg = {     21:3,
+                   22:2,
+                   23:2,
+                   24:3,
+                   25:3,
+                   26:3,
+                   27:3,
+                   28:14,
+                   29:3,
+                   30:3,
+                   31:3,
+                   35:7,
+                   36:17,
+                   37:18,
+                   38:16,
+                   39:1,
+                   40:13,
+                   41:9,
+                   42:1,
+                   43:1,
+                   44:1,
                    }
 
 
-# BB water classes to PALM water types
-bb2palmwat =    {   33:2,    #fliessgewaesser > river
-                    32:1,   #stehendes gewaesser > lake
+
+# land use water classes to PALM water types
+bb2palmwat =    {   16:4,
+                    32:1,
+                    33:2,
+                    34:1,
                 }
 
 
 # pavement file 8,9,10,11,12,13,14,15,17,18,19,20
-pav2palmpav = {  -9999.:mst.fillvalues['pavement_type'],
-                    8:  1, 
-                    9:  2,
-                    10: 1,
-                    11: 4,
-                    12: 2,
-                    13: 2,
-                    14: 9,
-                    15: 3,
-                    17: 1,
-                    18: 2,
-                    19: 13,
-                    20: 1,
-                    2001:1,
-                    2002:1,
-                    2003:1,
-                    
+pav2palmpav = {-9999.:mst.fillvalues['pavement_type'],
+                    8:1,
+                    9:2,
+                    10:1,
+                    11:4,
+                    12:2,
+                    13:2,
+                    14:9,
+                    15:3,
+                    17:1,
+                    18:2,
+                    19:13,
+                    20:1,
                   }
+
 
 
 #  street !only! Objektart to PALM street type
@@ -104,14 +106,23 @@ palmveg2palmsoil = {    mst.fillvalues['vegetation_type']:mst.fillvalues['soil_t
                         1:1,
                         2:6,
                         3:1,
+                        4:2,
+                        5:2,
+                        6:2,
+                        7:2,
                         8:2,
                         9:1,
+                        10:3,
+                        11:1,
+                        12:3,
                         13:1,
                         14:1,
+                        15:2,
                         16:2,
                         17:1,
                         18:1,
                     }
+
 
 
 # assign PALM soil type to PALM pavement types
